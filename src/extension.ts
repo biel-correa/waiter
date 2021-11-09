5ccb6c53355da26868ef6248f377b7ba0dfee173
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-11-09 15:43:47 -0300

    Feature: run the commands inside the config file

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,29 @@
 import * as vscode from 'vscode';
 
+interface ConfigFile {
+	tabs: Tab[]
+}
+
+interface Tab {
+	tabName: string
+	commands: string[]
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	console.log('Waiter is up and running!');
-
 	let disposable = vscode.commands.registerCommand('waiter.startEnvironment', () => {
 		//search for file
 		let configFilePath = vscode.workspace.rootPath + '\\waiter.config.json';
 		vscode.workspace.openTextDocument(configFilePath).then(doc => {
 			//load config
-			vscode.window.showTextDocument(doc);
+			let config: ConfigFile = JSON.parse(doc.getText());
+			config.tabs.forEach((tab: Tab) => {
+				let terminal = vscode.window.createTerminal(tab.tabName);
+				tab.commands.forEach((command: string) => {
+					terminal.sendText(command);
+				});
+			});
 		}, async (error: Error) => {
 			//create file when it doesn't exist
 			let action = await vscode.window.showErrorMessage("Could not find waiter.config.json.", 'Create File');
