23121279a70fc07b9ffa26658050d8a1b11b3b24
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-11-09 15:21:09 -0300

    Feature: Case config file doesn't exist, ask to create one
    Also added a structure for the config file

--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,25 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Waiter is up and running!');
 
 	let disposable = vscode.commands.registerCommand('waiter.startEnvironment', () => {
-		const terminal = vscode.window.createTerminal('Waiter');
-		terminal.sendText("echo 'New terminal created'");
+		//search for file
+		let configFilePath = vscode.workspace.rootPath + '\\waiter.config.json';
+		vscode.workspace.openTextDocument(configFilePath).then(doc => {
+			//load config
+			vscode.window.showTextDocument(doc);
+		}, async (error: Error) => {
+			//create file when it doesn't exist
+			let action = await vscode.window.showErrorMessage("Could not find waiter.config.json.", 'Create File');
+			if (action === 'Create File') {
+				var setting: vscode.Uri = vscode.Uri.parse("untitled:" + configFilePath);
+				vscode.workspace.openTextDocument(setting).then((a: vscode.TextDocument) => {
+					vscode.window.showTextDocument(a, 1, false).then(e => {
+						e.edit(edit => {
+							edit.insert(new vscode.Position(0, 0), '{\n\t"tabs": [\n\t\t{\n\t\t\t"tabName": "Custom tab name",\n\t\t\t"commands" : [\n\t\t\t\t"echo \'Hello world\'"\n\t\t\t]\n\t\t}\n\t]\n}');
+						});
+					});
+				});
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
